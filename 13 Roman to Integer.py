31171c8355d3673a51a3e722b2d807b54c7bc108
Eric Yang <Eric1eric2eric3eric4eric5@outlook.com>
2022-08-11 15:46:36 -0400

    Added question #13 Roman to Integer and my whiteboard planning for that question

--- 13 Roman to Integer.py
+++ 13 Roman to Integer.py
@@ -1,6 +1,5 @@
-
-if __name__ == '__main__':
-    def convertToInt( s: str) -> int:
+class Solution:
+    def convertToInt(self, s: str) -> int:
         if s == "I": return 1
         if s == "V": return 5
         if s == "X": return 10
@@ -9,28 +8,20 @@ if __name__ == '__main__':
         if s == "D": return 500
         if s == "M": return 1000
 
-
-    def romanToInt( s: str) -> int:
+    def romanToInt(self, s: str) -> int:
         sum = 0
         i = 0
         while i < len(s):
-            print("heyy ", i)
-            cur = convertToInt(s[i])
+            cur = self.convertToInt(s[i])
             if i < len(s) - 1:
-                cur = convertToInt(s[i])
-                next = convertToInt(s[i + 1])
+                next = self.convertToInt(s[i+1])
                 # Subtraction cases
                 if cur < next:
                     sum += (next - cur)
                     i += 1
-
                 else:
                     sum += cur
             else:
                 sum += cur
-
             i += 1
         return sum
-
-    print(romanToInt("IV"))
-
